aaca11b8a3f61337febafc0dec42760af6b5d840
Theo <ekunnusitheophilus@gmail.com>
2022-11-23 18:24:05 +0100

    [Feat] stats endpoint.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ Index view """
+from flask import json
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
+           "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
 def status():
-    return {"status": "Ok"}
+    """ returns status of Ok! """
+    return ({"status": "Ok"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ An endpoint that retrieves the number of each objects by type """
+    newObj = {}
+    for key, value in classes.items():
+        newObj[key] = storage.count(value)
+
+    return newObj
